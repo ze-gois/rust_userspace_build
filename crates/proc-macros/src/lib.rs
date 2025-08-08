5b82ccc1d5f4aee65d43818858bcfe58cbbc4f7f
José Gois <ze.gois.00@gmail.com>
2025-08-08 10:50:19 -0300

    licence

--- crates/proc-macros/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use proc_macro::TokenStream;
-
-mod result;
-
-#[proc_macro]
-pub fn define_error(input: TokenStream) -> TokenStream {
-    result::error::expand_define_error(input)
-}
--- crates/proc-macros/src/result/error.rs
+++
@@ -1,8 +0,0 @@
-mod generate;
-mod model;
-mod parser;
-
-pub fn expand_define_error(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let input = syn::parse_macro_input!(input as model::Error);
-    generate::generate_define_error(input).into()
-}
